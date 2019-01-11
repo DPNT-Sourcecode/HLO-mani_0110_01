6a088920aa4550e0ddd9e101b4d528f6a7fd7ae5
sbx_user1059 <sbx_user1059@ip-10-129-166-240.eu-west-2.compute.internal>
2019-01-11 00:14:57 +0000

    Fri Jan 11 00:14:57 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,8 +4,6 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        
-	return "Hello, World!";
-   }
+        throw new SolutionNotImplementedException();
+    }
 }
-
